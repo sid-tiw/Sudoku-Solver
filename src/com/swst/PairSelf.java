3f08ab5bafc34174cc28d04ea4c5549e8e2fa467
sid-tiw <201851127@iiitvadodara.ac.in>
2019-09-28 13:09:19 +0530

    Add Files Via Upload

---
+++ src/com/swst/PairSelf.java
@@ -0,0 +1,10 @@
+package com.swst;
+
+public class PairSelf {                         //int, int pair customized for usage in this project
+    public int first, second;
+    public PairSelf(int first, int second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+}
